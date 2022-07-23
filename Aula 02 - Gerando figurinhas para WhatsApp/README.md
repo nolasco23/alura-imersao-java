c70681ccac61d71ad55908942e7a76f801546098
nolasco23 <renatorochanolasco@hotmail.com>
2022-07-23 14:55:45 -0300

    Gerando figurinhas para WhatsApp

---
+++ Aula 02 - Gerando figurinhas para WhatsApp/README.md
@@ -0,0 +1,22 @@
+# Alura Stickers - Imersão Java - Alura
+
+> Projeto desenvolvido durante a semana de Imersão Java da Alura durante os dias 18 a 22 de julho de 2022.
+
+## Aula 2 👨‍💻⭐⭐
+
+- Gerando figurinhas para WhatsApp
+
+### Desafios desta aula ✨⭐⭐
+
+- Criar um gerador de figurinhas explorando outras bibliotecas nativas do Java, para enviar por Whatsapp os poster dos filmes preferidos!
+
+1. Centralizar o texto na figurinha.
+2. Fazer um pacote no Whatsapp e/ou Telegram com as suas próprias figurinhas!
+3. Criar diretório de saída das imagens, se ainda não existir.
+4. Colocar outra fonte como a Comic Sans ou a Impact, a fonte usada em memes.
+5. Colocar uma imagem de você que está fazendo esse curso sorrindo, fazendo joinha!
+6. Colocar contorno (outline) no texto da imagem.
+7. Tratar as imagens retornadas pela API do IMDB para pegar uma imagem maior ao invés dos thumbnails. Opções: pegar a URL da imagem e remover o trecho mostrado durante a aula ou consumir o endpoint de posters da API do IMDB (mais trabalhoso), tratando o JSON retornado.
+8. Fazer com que o texto da figurinha seja personalizado de acordo com as classificações do IMDB.
+9. Desafio supremo: usar alguma biblioteca de manipulação de imagens como OpenCV pra extrair imagem principal e contorná-la.
+10. Alterar texto de "Topzera" para a "Classicação do filme"
---
+++ Aula 02 - Gerando figurinhas para WhatsApp/src/App.java
@@ -0,0 +1,59 @@
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Top250Data
+        // String url = "https://imdb-api.com/en/API/Top250Movies/k_kdi6x9hg";
+
+        // MostPopularData
+        // String url = "https://imdb-api.com/en/API/MostPopularMovies/k_kdi6x9hg";
+        String url = "https://api.mocki.io/v2/549a5d8b";
+
+        URI endereco = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+
+        var parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+
+        for (Map<String, String> filme : listaDeFilmes.subList(0, listaDeFilmes.size())) {
+            String texto = "Classificação: ";
+            System.out.println("Título: " + filme.get("title"));
+            System.out.println("Poster: " + filme.get("image"));
+            
+            if (filme.get("imDbRating") == null || filme.get("imDbRating").length() == 0) {
+                System.out.print("\u2620");
+            } else {
+                var numberRating = (int) Math.round(Double.parseDouble(filme.get("imDbRating")));
+                // System.out.print("Classificação: ");
+                for (int j = 0; j < numberRating; j++) {
+                    // System.out.print("\u2B50");
+                    texto += "\u2B50";
+                }
+                // System.out.print("\u001b[97m \u001b[104m" + filme.get("imDbRating") + "
+                // \u001b[m");
+
+                texto += "\u001b[97m \u001b[104m" + filme.get("imDbRating") + " \u001b[m";
+                System.out.println(texto);
+                
+                String urlImage = filme.get("image");
+                InputStream inputStream = new URL(urlImage).openStream();
+                var generate = new GeradoraDeFigurinhas();
+                generate.create(inputStream, filme.get("title"), "Classificação: " + filme.get("imDbRating"));
+
+            }
+            System.out.println("\n");
+
+        }
+    }
+}
